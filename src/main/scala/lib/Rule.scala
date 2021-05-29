071a5f5460d73fe4a0f0a14d56f53235cdb7fb3a
Mathis Randl <mathis.randl@epfl.ch>
2021-05-29 23:40:15 +0200

    changed list to sets for performance

--- src/main/scala/lib/Rule.scala
+++ src/main/scala/lib/Rule.scala
@@ -1,9 +1,9 @@
 package lib
 
-case class Rule(conditions : List[Proposition], conclusion : Proposition) {
+case class Rule(conditions : Set[Proposition], conclusion : Proposition) {
 
-  def dependsOn(fact : Proposition) : List[(Proposition, Environment)] = 
-    conditions.map(cond => (cond, Unifier.patternMatch(fact, cond, None))).filter(x => x(1).isDefined).map(x => (x(0), x(1).get))
+  def dependsOn(fact : Proposition) : Set[Environment] = 
+    conditions.map(cond => Unifier.patternMatch(fact, cond, None)).filter(_.isDefined).map(_.get)
   
   def satisifiedBy(facts : Set[Proposition], env : Environment) =
     conditions.foldLeft(Set(env)){(envs, cond) => 
