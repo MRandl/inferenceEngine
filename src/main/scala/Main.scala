2b78da3c2961865b4f184d4d104c383f02dbd43d
Mathis Randl <mathis.randl@epfl.ch>
2021-05-29 23:40:47 +0200

    added basic main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,17 @@
 import lib.*
 
 @main def hello: Unit = 
-  val rule = Rule(List(Constant("flies")), Constant("bird"))
-  val constant = Constant("flies")
+  val flies = Constant("flies")
+  val swims = Constant("swims")
+  val duck  = Constant("duck")
+  val fish  = Constant("fish")
+  val alien = Constant("alien")
+
+  val duckFlies = Rule(Set(duck), flies)
+  val duckSwims = Rule(Set(duck), swims)
+  val fishSwims = Rule(Set(fish), swims)
+  
   println(
-    lib.engines.ForwardsChainingEngine.runWithoutTarget(List(constant), Set(rule))
+    lib.engines.ForwardsChainingEngine.runWithoutTarget(List(duck), Set(duckFlies, fishSwims, duckSwims))
   )
   System.out.flush
