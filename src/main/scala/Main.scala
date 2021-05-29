21117968ce8081fbae3cfa294e5d69002ca03b3e
Mathis Randl <mathis.randl@epfl.ch>
2021-05-30 00:30:23 +0200

    added toy example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,28 @@
 import lib.*
 
 @main def hello: Unit = 
-  val flies = Constant("flies")
-  val swims = Constant("swims")
-  val duck  = Constant("duck")
-  val fish  = Constant("fish")
-  val alien = Constant("alien")
+  
+  val duck  = Composite("being", List(Constant("duck")))
+  val fish  = Composite("being", List(Constant("fish")))
+  val alien = Composite("being", List(Constant("alien")))
 
-  val duckFlies = Rule(Set(duck), flies)
-  val duckSwims = Rule(Set(duck), swims)
-  val fishSwims = Rule(Set(fish), swims)
+  val knowsIsTransitive  = Rule(Set(knows("x", "y"), knows("y", "z")), knows("x", "z"))
+  val knowsIsReflexive   = Rule(Set(being("x")), Composite("knows", List(Variable("x"), Variable("x"))))
+  val knowsIsCommutative = Rule(Set(knows("x", "y")), knows("y", "x"))
+
+  val duckKnowsFish  = Composite("knows", List(Constant("duck"), Constant("fish")))
+  val fishKnowsAlien = Composite("knows", List(Constant("fish"), Constant("alien")))
   
   println(
-    lib.engines.ForwardsChainingEngine.runWithoutTarget(List(duck), Set(duckFlies, fishSwims, duckSwims))
+    lib.engines.ForwardsChainingEngine.runWithoutTarget(
+      Set(duckKnowsFish, fishKnowsAlien, duck, fish, alien), 
+      Set(knowsIsTransitive, knowsIsReflexive, knowsIsCommutative)
+    ).mkString("\n")
   )
   System.out.flush
+
+def knows(x : String, y : String) = 
+  Composite("knows", List(Variable(x), Variable(y)))
+
+def being(x : String) = 
+  Composite("being", List(Variable(x)))
