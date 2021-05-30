2781957da9212f7573e6851bbbff52ea787763bd
Mathis Randl <mathis.randl@epfl.ch>
2021-05-30 11:46:12 +0200

    slightly complexified the example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,17 @@
 import lib.*
 
 @main def hello: Unit = 
-  
-  val duck  = Composite("being", List(Constant("duck")))
-  val fish  = Composite("being", List(Constant("fish")))
-  val alien = Composite("being", List(Constant("alien")))
 
   val knowsIsTransitive  = Rule(Set(knows("x", "y"), knows("y", "z")), knows("x", "z"))
-  val knowsIsReflexive   = Rule(Set(being("x")), Composite("knows", List(Variable("x"), Variable("x"))))
   val knowsIsCommutative = Rule(Set(knows("x", "y")), knows("y", "x"))
 
   val duckKnowsFish  = Composite("knows", List(Constant("duck"), Constant("fish")))
   val fishKnowsAlien = Composite("knows", List(Constant("fish"), Constant("alien")))
-  
+  val lamaKnowsAlien = Composite("knows", List(Constant("lama"), Constant("alien")))
+
   println(
     lib.engines.ForwardsChainingEngine.runWithoutTarget(
-      Set(duckKnowsFish, fishKnowsAlien), 
+      Set(duckKnowsFish, fishKnowsAlien, lamaKnowsAlien), 
       Set(knowsIsTransitive, knowsIsCommutative)
     ).map(_.toString).toList.sorted.mkString("\n")
   )
